a1322722dc1d9d7894df899e0c1fb359fcb4b28f
Ngetikin <ngetikin.agency@gmail.com>
2025-10-27 14:13:14 +0700

    add: husky pre-push, gbuild apphtml-css-js,Github action for github pages, fix: husky pre-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
-import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+    rules: {
+      // 🔧 Matikan rule yang terlalu ketat untuk normalize.css
+      "css/font-family-fallbacks": "off",
+    },
+  },
 ]);
